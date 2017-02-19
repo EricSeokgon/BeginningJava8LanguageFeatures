e802040a6a112bae25f6690500301c81f53db94e
sklee <hadesleeq@gmail.com>
2017-02-20 08:47:55 +0900

    Initial commit

---
+++ src/chapter1/Employee.java
@@ -0,0 +1,16 @@
+package chapter1;
+
+/**
+ * Project: BeginningJava8LanguageFeatures
+ * FileName: Employee
+ * Date: 2017-02-20
+ * Time: 오전 8:46
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Employee {
+    public void setSalary(double salary) {
+        System.out.println("Employee.setSalary(): "+salary);
+    }
+}
