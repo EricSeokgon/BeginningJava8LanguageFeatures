846e94fb47a01c5d24abf8e4f5e9f748ed195f0f
sklee <hadesleeq@gmail.com>
2017-02-21 08:39:31 +0900

    Declaring an Annotation Type

---
+++ src/chapter1/VersionTest.java
@@ -0,0 +1,15 @@
+package chapter1;
+
+/**
+ * Project: BeginningJava8LanguageFeatures
+ * FileName: VersionTest
+ * Date: 2017-02-21
+ * Time: 오전 8:38
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+@Version(major = 1, minor = 0)
+public class VersionTest {
+
+}
