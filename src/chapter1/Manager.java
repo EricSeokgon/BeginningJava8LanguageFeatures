a18c428733e9fd6426c1ad0e7a788667ec4a7286
sklee <hadesleeq@gmail.com>
2017-02-20 09:08:15 +0900

    setSalary() method

---
+++ src/chapter1/Manager.java
@@ -0,0 +1,16 @@
+package chapter1;
+
+/**
+ * Project: BeginningJava8LanguageFeatures
+ * FileName: Manager
+ * Date: 2017-02-20
+ * Time: 오전 9:06
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Manager extends Employee {
+    public void setSalary(int salary) {
+        System.out.println("Manager.setSalary(): " + salary);
+    }
+}
