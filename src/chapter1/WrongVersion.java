e5e38a7f7a87732413ece2fc263b550942cd8989
sklee <hadesleeq@gmail.com>
2017-02-23 08:36:27 +0900

    Restriction #1

---
+++ src/chapter1/WrongVersion.java
@@ -0,0 +1,14 @@
+package chapter1;
+
+/**
+ * Project: BeginningJava8LanguageFeatures
+ * FileName: WrongVersion
+ * Date: 2017-02-23
+ * Time: 오전 8:35
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public @interface WrongVersion extends BasicVersion {
+    int extended();
+}
