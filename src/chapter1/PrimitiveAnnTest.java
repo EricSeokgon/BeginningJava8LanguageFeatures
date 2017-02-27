9a47d0e9b3a043bc80817fdb8ce9c479c16afc92
sklee <hadesleeq@gmail.com>
2017-02-28 08:47:29 +0900

    Primitive Types

---
+++ src/chapter1/PrimitiveAnnTest.java
@@ -0,0 +1,21 @@
+package chapter1;
+
+/**
+ * Project: BeginningJava8LanguageFeatures
+ * FileName: PrimitiveAnnTest
+ * Date: 2017-02-28
+ * Time: 오전 8:46
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public @interface PrimitiveAnnTest {
+    byte a();
+    short b();
+    int c();
+    long d();
+    float e();
+    double f();
+    boolean g();
+    char h();
+}
