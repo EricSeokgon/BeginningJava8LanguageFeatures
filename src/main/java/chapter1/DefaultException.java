cff4293395a3b713adb35324971924a3c854eaac
sklee <hadesleeq@gmail.com>
2017-03-03 10:41:46 +0900

    Class Types

---
+++ src/main/java/chapter1/DefaultException.java
@@ -0,0 +1,21 @@
+package chapter1;
+
+/**
+ * Project: BeginningJava8LanguageFeatures
+ * FileName: DefaultException
+ * Date: 2017-03-03
+ * Time: 오전 10:40
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class DefaultException extends java.lang.Throwable {
+    public DefaultException() {
+
+    }
+
+    public DefaultException(String msg) {
+        super(msg);
+    }
+
+}
