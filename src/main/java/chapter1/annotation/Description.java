6de26abdfe0372f1444d2319d117bdbf8811014c
sklee <hadesleeq@gmail.com>
2017-03-07 08:54:27 +0900

    Annotation Type

--- src/main/java/chapter1/annotation/Description.java
+++ src/main/java/chapter1/annotation/Description.java
@@ -2,14 +2,15 @@ package chapter1.annotation;
 
 /**
  * Project: BeginningJava8LanguageFeatures
- * FileName: Name
- * Date: 2017-03-02
- * Time: 오전 8:53
+ * FileName: Description
+ * Date: 2017-03-07
+ * Time: 오전 8:52
  * Author: Hadeslee
  * Note:
  * To change this template use File | Settings | File Templates.
  */
-public @interface Name {
-    String first();
-    String last();
+public @interface Description {
+    Name name();
+    Version version();
+    String comments() default "";
 }
