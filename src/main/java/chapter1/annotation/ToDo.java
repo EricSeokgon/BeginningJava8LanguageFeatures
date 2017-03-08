d1966e906746bfe2f54421333dae0fcc0d38835b
sklee <hadesleeq@gmail.com>
2017-03-08 09:37:42 +0900

    ToDo Annotation Type with String[ ] as Its Sole Element

---
+++ src/main/java/chapter1/annotation/ToDo.java
@@ -0,0 +1,14 @@
+package chapter1.annotation;
+
+/**
+ * Project: BeginningJava8LanguageFeatures
+ * FileName: ToDo
+ * Date: 2017-03-08
+ * Time: 오전 9:37
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public @interface ToDo {
+    String[] items();
+}
