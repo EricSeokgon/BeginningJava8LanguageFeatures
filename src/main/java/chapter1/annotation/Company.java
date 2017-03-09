43852644a28e00b28bbb61766c2d38bb5914a136
sklee <hadesleeq@gmail.com>
2017-03-10 08:41:25 +0900

    Shorthand Annotation Syntax

---
+++ src/main/java/chapter1/annotation/Company.java
@@ -0,0 +1,14 @@
+package chapter1.annotation;
+
+/**
+ * Project: BeginningJava8LanguageFeatures
+ * FileName: Company
+ * Date: 2017-03-10
+ * Time: 오전 8:40
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public @interface Company {
+    String value(); //the element name is value
+}
