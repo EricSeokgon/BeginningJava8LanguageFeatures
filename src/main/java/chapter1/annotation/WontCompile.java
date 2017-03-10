bcdb3b62ad8b519fa2812dc58f10b7734a14210e
sklee <hadesleeq@gmail.com>
2017-03-10 16:46:45 +0900

    Meta-Annotation Types

---
+++ src/main/java/chapter1/annotation/WontCompile.java
@@ -0,0 +1,16 @@
+package chapter1.annotation;
+
+/**
+ * Project: BeginningJava8LanguageFeatures
+ * FileName: WontCompile
+ * Date: 2017-03-10
+ * Time: 오후 4:45
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class WontCompile {
+    //@Version(major = 1, minor = 1)
+    int id = 110;
+}
+
