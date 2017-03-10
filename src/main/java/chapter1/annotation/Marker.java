7975fd9da2fa31152d2dfa0ad0b73e2d5452003e
sklee <hadesleeq@gmail.com>
2017-03-10 16:40:25 +0900

    Marker Annotation Types

---
+++ src/main/java/chapter1/annotation/Marker.java
@@ -0,0 +1,14 @@
+package chapter1.annotation;
+
+/**
+ * Project: BeginningJava8LanguageFeatures
+ * FileName: Marker
+ * Date: 2017-03-10
+ * Time: 오후 4:40
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public @interface Marker {
+
+}
