4560ec6012d7dc233f4a299385208218e71b3f91
sklee <hadesleeq@gmail.com>
2017-03-10 16:53:23 +0900

    The Inherited Annotation Type

---
+++ src/main/java/chapter1/annotation/Ann2.java
@@ -0,0 +1,14 @@
+package chapter1.annotation;
+
+/**
+ * Project: BeginningJava8LanguageFeatures
+ * FileName: Ann2
+ * Date: 2017-03-10
+ * Time: 오후 4:53
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public @interface Ann2 {
+    int id();
+}
