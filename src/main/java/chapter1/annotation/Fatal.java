2cd5fb2f28cabc15ab8890e4d9a936e3926741cf
sklee <hadesleeq@gmail.com>
2017-03-10 16:51:05 +0900

    The Retention Annotation

---
+++ src/main/java/chapter1/annotation/Fatal.java
@@ -0,0 +1,14 @@
+package chapter1.annotation;
+
+/**
+ * Project: BeginningJava8LanguageFeatures
+ * FileName: Fatal
+ * Date: 2017-03-10
+ * Time: 오후 4:48
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+
+public @interface Fatal {
+}
--- src/main/java/chapter1/annotation/Version.java
+++ src/main/java/chapter1/annotation/Version.java
@@ -1,6 +1,8 @@
 package chapter1.annotation;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
@@ -14,6 +16,7 @@ import java.lang.annotation.Target;
  */
 
 @Target({ElementType.TYPE, ElementType.CONSTRUCTOR, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Version {
     int major();
     int minor();
