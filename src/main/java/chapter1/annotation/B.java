f5a2c9e5e3a9aa35d8cf3d4fcd6ac2fc0f802160
sklee <hadesleeq@gmail.com>
2017-03-10 16:57:14 +0900

    The Inherited Annotation Type

--- src/main/java/chapter1/annotation/B.java
+++ src/main/java/chapter1/annotation/B.java
@@ -2,16 +2,13 @@ package chapter1.annotation;
 
 /**
  * Project: BeginningJava8LanguageFeatures
- * FileName: A
+ * FileName: B
  * Date: 2017-03-10
- * Time: 오후 4:38
+ * Time: 오후 4:54
  * Author: user
  * Note:
  * To change this template use File | Settings | File Templates.
  */
-@Ann2(id=505)
-@Ann3(id=707)
-public @interface A {
-    String value();
-    int id() default 10;
+public abstract class B implements A {
+
 }
