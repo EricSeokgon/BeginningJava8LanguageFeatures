a2c5307806a8daa8ad9f6f1550867e749de6b2f0
sklee <hadesleeq@gmail.com>
2017-03-14 15:34:10 +0900

    An Example of a Top-Level Class

---
+++ src/main/java/chapter2/Outer.java
@@ -0,0 +1,15 @@
+package chapter2;
+
+/**
+ * Project: BeginningJava8LanguageFeatures
+ * FileName: Outer
+ * Date: 2017-03-14
+ * Time: 오후 3:33
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Outer {
+    public class Inner {
+    }
+}
