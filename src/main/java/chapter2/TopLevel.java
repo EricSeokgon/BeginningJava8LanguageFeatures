5cc38578dc9f0f559de4e187c4f6bfbc848dc593
sklee <hadesleeq@gmail.com>
2017-03-14 15:33:42 +0900

    An Example of a Top-Level Class

---
+++ src/main/java/chapter2/TopLevel.java
@@ -0,0 +1,22 @@
+package chapter2;
+
+/**
+ * Project: BeginningJava8LanguageFeatures
+ * FileName: TopLevel
+ * Date: 2017-03-14
+ * Time: 오후 3:33
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class TopLevel {
+    private int value = 101;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
