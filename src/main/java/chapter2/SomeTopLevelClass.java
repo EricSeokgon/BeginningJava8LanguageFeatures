491e8205c83eaf275bfceb97fe6c29ad4166f6c9
sklee <hadesleeq@gmail.com>
2017-03-15 09:22:29 +0900

    Using a Local Inner Class

---
+++ src/main/java/chapter2/SomeTopLevelClass.java
@@ -0,0 +1,20 @@
+package chapter2;
+
+/**
+ * Project: BeginningJava8LanguageFeatures
+ * FileName: SomeTopLevelClass
+ * Date: 2017-03-15
+ * Time: 오전 9:21
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class SomeTopLevelClass {
+    // Some code for SomeTopLevelClass goes here
+    public void someMethod() {
+        class SomeLocalInnerClass {
+            // Some code for SomeLocalInnerClass goes here
+        }
+        // SomeLocalInnerClass can only be used here
+    }
+}
