e6b80c94c845609a7582f6ec55d4742bf84bc91c
sklee <hadesleeq@gmail.com>
2017-03-23 17:41:05 +0900

    Use of the Keyword this Qualified with the Class Name

---
+++ src/main/java/chapter2/innerclasses/TopLevelOuter.java
@@ -0,0 +1,26 @@
+package chapter2.innerclasses;
+
+/**
+ * Project: BeginningJava8LanguageFeatures
+ * FileName: TopLevelOuter
+ * Date: 2017-03-23
+ * Time: 오후 5:14
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class TopLevelOuter {
+    private int v1 = 100;
+
+    public class InnerLevelOne {
+        private int v2 = 200;
+
+        public class InnerLevelTwo {
+            private int v3 = 300;
+
+            public class InnerLevelThere {
+                private int v4 = 400;
+            }
+        }
+    }
+}
