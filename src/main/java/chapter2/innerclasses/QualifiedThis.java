939758ea76c95d743c2f3a5295effa8c813d2068
sklee <hadesleeq@gmail.com>
2017-03-23 17:11:17 +0900

    Use of the Keyword this Qualified with the Class Name

--- src/main/java/chapter2/innerclasses/QualifiedThis.java
+++ src/main/java/chapter2/innerclasses/QualifiedThis.java
@@ -11,11 +11,13 @@ package chapter2.innerclasses;
  */
 public class QualifiedThis {
     private int value = 828;
+
     public void printValue() {
         System.out.println("value = " + value);
         System.out.println("this.value = " + this.value);
         System.out.println("QualifiedThis.this.value= " + QualifiedThis.this.value);
     }
+
     public void printHiddenValue() {
         int value = 131;
         System.out.println("value = " + value);
