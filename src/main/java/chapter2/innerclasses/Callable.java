87e3728b7a337151801909db143df6d84616ebee
sklee <hadesleeq@gmail.com>
2017-03-28 11:16:27 +0900

    A Callable Interface to Implement a Callback Mechanism

---
+++ src/main/java/chapter2/innerclasses/Callable.java
@@ -0,0 +1,14 @@
+package chapter2.innerclasses;
+
+/**
+ * Project: BeginningJava8LanguageFeatures
+ * FileName: Callable
+ * Date: 2017-03-28
+ * Time: 오전 11:15
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Callable {
+    void call();
+}
