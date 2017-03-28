cb7e66d80b451706587c07d9e28ac6319eec0f5d
sklee <hadesleeq@gmail.com>
2017-03-28 11:30:16 +0900

    A Bulb Class to Demonstrate Initialization of a Class

---
+++ src/main/java/chapter3/reflection/Bulb.java
@@ -0,0 +1,18 @@
+package chapter3.reflection;
+
+/**
+ * Project: BeginningJava8LanguageFeatures
+ * FileName: Bulb
+ * Date: 2017-03-28
+ * Time: 오전 11:29
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Bulb {
+    static {
+        System.out.println("Loading class Bulb...");
+    }
+}
+
+
