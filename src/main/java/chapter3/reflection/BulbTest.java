1e3a22c2d2530ef69ee3e848318dca23771a951b
sklee <hadesleeq@gmail.com>
2017-03-29 11:01:56 +0900

    Testing Class Loading and Initialization

---
+++ src/main/java/chapter3/reflection/BulbTest.java
@@ -0,0 +1,58 @@
+package chapter3.reflection;
+
+/**
+ * Project: BeginningJava8LanguageFeatures
+ * FileName: BulbTest
+ * Date: 2017-03-29
+ * Time: 오전 10:59
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class BulbTest {
+    public static void main(String[] args) {
+    /* Uncomment only one of the following statements at a time.
+       Observe the output to see the difference in the way the Bulb class
+       is loaded and initialized.
+    */
+        BulbTest.createObject();
+        // BulbTest.forName();
+        // BulbTest.forNameVersion2();
+        // BulbTest.classLiteral();
+    }
+    public static void classLiteral() {
+        // Will load the class, but won't initialize in Java 5 and later.
+        // Prior version of Java will initialize the class too.
+        Class<Bulb> c = Bulb.class;
+    }
+
+    public static void forNameVersion2() {
+        try {
+            String className = "com.jdojo.reflection.Bulb";
+            boolean initialize = false;
+            // Get the classloader for the current class
+            ClassLoader cLoader = BulbTest.class.getClassLoader();
+            // Will load, but not initialize the class, because we have
+            // set the initialize variable to false
+            Class c = Class.forName(className, initialize, cLoader);
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void forName() {
+        try {
+            String className = "com.jdojo.reflection.Bulb";
+            // Will load and initialize the class
+            Class c = Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void createObject() {
+        // Will load and initialize the Bulb class
+        new Bulb();
+    }
+
+}
