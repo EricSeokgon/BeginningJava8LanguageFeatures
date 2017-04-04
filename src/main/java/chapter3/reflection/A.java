cf75bb988c47904e410e1cc7ca22e2a578135208
sklee <hadesleeq@gmail.com>
2017-04-04 13:47:50 +0900

    Reflecting on Fields

---
+++ src/main/java/chapter3/reflection/A.java
@@ -0,0 +1,16 @@
+package chapter3.reflection;
+
+/**
+ * Project: BeginningJava8LanguageFeatures
+ * FileName: A
+ * Date: 2017-04-04
+ * Time: 오후 1:47
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class A implements IConstants {
+    private int aPrivate;
+    public int aPublic;
+    protected int aProtected;
+}
