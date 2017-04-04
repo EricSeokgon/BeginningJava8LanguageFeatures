3dc01dd0723a87d34567a1f4cc5a08c15270c963
sklee <hadesleeq@gmail.com>
2017-04-04 13:48:41 +0900

    Reflecting on Fields

---
+++ src/main/java/chapter3/reflection/B.java
@@ -0,0 +1,16 @@
+package chapter3.reflection;
+
+/**
+ * Project: BeginningJava8LanguageFeatures
+ * FileName: B
+ * Date: 2017-04-04
+ * Time: 오후 1:48
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class B extends A {
+    private int bPrivate;
+    public int bPublic;
+    protected int bProtected;
+}
