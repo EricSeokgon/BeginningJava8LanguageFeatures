69e8f3c35f5d358270686e74076ada879983e681
sklee <hadesleeq@gmail.com>
2017-04-04 13:46:44 +0900

    Reflecting on Fields

---
+++ src/main/java/chapter3/reflection/IConstants.java
@@ -0,0 +1,14 @@
+package chapter3.reflection;
+
+/**
+ * Project: BeginningJava8LanguageFeatures
+ * FileName: IConstants
+ * Date: 2017-04-04
+ * Time: 오후 1:46
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public interface IConstants {
+    int DAYS_IN_WEEK = 7;
+}
