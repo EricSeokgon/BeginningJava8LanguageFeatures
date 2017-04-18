c2fe4ba89885bb870d3b8bf17fd4f61f8377afcc
sklee <hadesleeq@gmail.com>
2017-04-18 09:54:42 +0900

    Unbounded Wildcards

---
+++ src/main/java/chapter4/generics/WrapperUtil.java
@@ -0,0 +1,16 @@
+package chapter4.generics;
+
+/**
+ * Project: BeginningJava8LanguageFeatures
+ * FileName: WrapperUtil
+ * Date: 2017-04-18
+ * Time: 오전 9:54
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class WrapperUtil {
+    public static void printDetails(Wrapper<Object> wrapper){
+        // More code goes here
+    }
+}
