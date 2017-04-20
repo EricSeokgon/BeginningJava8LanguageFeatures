180479419e6d9a95147f1c6d64c835c63e8dd5bc
sklee <hadesleeq@gmail.com>
2017-04-20 10:10:06 +0900

    Generic Methods and Constructors

---
+++ src/main/java/chapter4/generics/Test.java
@@ -0,0 +1,16 @@
+package chapter4.generics;
+
+/**
+ * Project: BeginningJava8LanguageFeatures
+ * FileName: Test
+ * Date: 2017-04-20
+ * Time: 오전 10:05
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test<T> {
+    public <V> void m1(Wrapper<V> a, Wrapper<V> b, T c) {
+// Do something
+    }
+}
