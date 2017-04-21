fd2491064ac26f121b114f6e0cc399b072b98af6
hadesyi <leesk55@gmail.com>
2017-04-22 00:55:34 +0900

    A WrapperUtil Utility Class That Works with Wrapper Objects

--- src/main/java/chapter4/generics/WrapperUtil.java
+++ src/main/java/chapter4/generics/WrapperUtil.java
@@ -20,6 +20,15 @@ public class WrapperUtil {
         System.out.println("Class: " + className);
         System.out.println("Value: " + value);
     }
+
+    public static double sum(Wrapper<? extends Number> n1,
+                             Wrapper<? extends Number> n2) {
+        Number num1 = n1.get();
+        Number num2 = n2.get();
+        double sum = num1.doubleValue() + num2.doubleValue();
+        return sum;
+    }
+
     public static <T> void copy(Wrapper<T> source, Wrapper<T> dest) {
         T value = source.get();
         dest.set(value);
