c76490dbf2302a323333cb7fa4a0ef5853057129
hadesyi <leesk55@gmail.com>
2017-05-03 02:38:48 +0900

    Using the @FunctionalInterface Annotation

---
+++ src/main/java/chapter5/lambda/Operations.java
@@ -0,0 +1,10 @@
+package chapter5.lambda;
+
+/**
+ * Created by hadeslee on 2017-05-03.
+ */
+
+public interface Operations {
+    double add(double n1, double n2);
+    double subtract(double n1, double n2);
+}
