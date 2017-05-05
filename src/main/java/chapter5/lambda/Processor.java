a423cbb1fd8d39ce8eab942c450b18e5aaa03187
hadesyi <leesk55@gmail.com>
2017-05-05 23:27:35 +0900

    Generic Functional Interface

--- src/main/java/chapter5/lambda/Processor.java
+++ src/main/java/chapter5/lambda/Processor.java
@@ -1,13 +1,7 @@
 package chapter5.lambda;
 
 /**
- * Project: BeginningJava8LanguageFeatures
- * FileName: Processor
- * Date: 2017-05-04
- * Time: 오전 9:05
- * Author: user
- * Note:
- * To change this template use File | Settings | File Templates.
+ * Created by hadeslee on 2017-05-05.
  */
 @FunctionalInterface
 public interface Processor {
