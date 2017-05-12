4227ff173df42c8710dc23272e3f0c737bf395f1
sklee <hadesleeq@gmail.com>
2017-05-12 10:48:49 +0900

    A Printer Functional Interface

---
+++ src/main/java/chapter5/lambda/Printer.java
@@ -0,0 +1,15 @@
+package chapter5.lambda;
+
+/**
+ * Project: BeginningJava8LanguageFeatures
+ * FileName: Printer
+ * Date: 2017-05-12
+ * Time: 오전 10:48
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+@FunctionalInterface
+public interface Printer {
+    void print(String msg);
+}
