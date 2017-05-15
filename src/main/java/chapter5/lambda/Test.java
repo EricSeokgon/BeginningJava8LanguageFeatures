3e124b3c768f0e6b3cc9bba7de0cc46e1884c1d6
hadesyi <leesk55@gmail.com>
2017-05-15 23:06:03 +0900

    Testing Scope of a Lambda Expression and an Anonymous Class

---
+++ src/main/java/chapter5/lambda/Test.java
@@ -0,0 +1,14 @@
+package chapter5.lambda;
+
+/**
+ * Created by hadeslee on 2017-05-15.
+ */
+public class Test {
+    public static void main(String[] args) {
+        String msg = "Hello";
+        Printer printer = msg1 -> {
+            String msg = "Hi"; // A compile-time error
+            System.out.println(msg1);
+        };
+    }
+}
