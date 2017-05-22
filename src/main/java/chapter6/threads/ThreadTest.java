f52abfa92c0d444327e35c15f299a633bc25edb2
sklee <hadesleeq@gmail.com>
2017-05-22 09:56:32 +0900

    Using a Method Referen

---
+++ src/main/java/chapter6/threads/ThreadTest.java
@@ -0,0 +1,16 @@
+package chapter6.threads;
+
+/**
+ * Project: BeginningJava8LanguageFeatures
+ * FileName: ThreadTest
+ * Date: 2017-05-22
+ * Time: 오전 9:56
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class ThreadTest {
+    public static void execute() {
+        System.out.println("Hello Java thread!");
+    }
+}
