00469768846521441d62814d7b3b8d3b9036ce95
sklee <hadesleeq@gmail.com>
2017-05-24 09:03:05 +0900

    Running Multiple Threads in a Program

---
+++ src/main/java/chapter6/threads/MultiPrinterThread.java
@@ -0,0 +1,26 @@
+package chapter6.threads;
+
+/**
+ * Project: BeginningJava8LanguageFeatures
+ * FileName: MultiPrinterThread
+ * Date: 2017-05-23
+ * Time: 오전 9:16
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class MultiPrinterThread {
+    public static void main(String[] args) {
+        // Create two Thread objects
+        Thread t1 = new Thread(MultiPrinterThread::print);
+        Thread t2 = new Thread(MultiPrinterThread::print);
+        // Start both threads
+        t1.start();
+        t2.start();
+    }
+    public static void print() {
+        for (int i = 1; i <= 500; i++) {
+            System.out.println(i);
+        }
+    }
+}
