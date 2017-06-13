ed4fc0a44bed5d53ff1f09544e454714c71b46ba
sklee <hadesleeq@gmail.com>
2017-06-13 10:49:42 +0900

    A Class That Demonstrates How to Use a CyclicBarrier in a Program

--- src/main/java/chapter6/threads/Runnable.java
+++ src/main/java/chapter6/threads/Runnable.java
@@ -4,6 +4,6 @@ package chapter6.threads;
  * Created by hadeslee on 2017-05-21.
  */
 @FunctionalInterface
-public interface Runnable {
+public interface Runnable extends java.lang.Runnable {
     void run();
 }
