31f7d004cee57ef1bd1d0108dd6df3ed4b55fdbb
sklee <hadesleeq@gmail.com>
2017-06-16 13:05:00 +0900

    A Rum Class

---
+++ src/main/java/chapter7/inputoutput/Rum.java
@@ -0,0 +1,18 @@
+package chapter7.inputoutput;
+
+/**
+ * Project: BeginningJava8LanguageFeatures
+ * FileName: Rum
+ * Date: 2017-06-16
+ * Time: 오후 1:03
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Rum extends Drink {
+    public Rum() {
+        this.name = "Rum";
+        this.price = 0.9;
+    }
+}
+
