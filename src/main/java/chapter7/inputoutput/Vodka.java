12f095d82ac388b212dfd33acee993add294fa35
sklee <hadesleeq@gmail.com>
2017-06-16 13:05:55 +0900

    A Vodka Class

---
+++ src/main/java/chapter7/inputoutput/Vodka.java
@@ -0,0 +1,17 @@
+package chapter7.inputoutput;
+
+/**
+ * Project: BeginningJava8LanguageFeatures
+ * FileName: Vodka
+ * Date: 2017-06-16
+ * Time: 오후 1:05
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Vodka extends Drink {
+    public Vodka() {
+        this.name = "Vodka";
+        this.price = 1.2;
+    }
+}
