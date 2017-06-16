cbe4693e9be21de0e429029f06c38f8a892e91af
sklee <hadesleeq@gmail.com>
2017-06-16 09:58:09 +0900

    An Abstract Drink Class to Model the Abstract Component in the Decorator Pattern

---
+++ src/main/java/chapter7/inputoutput/Drink.java
@@ -0,0 +1,21 @@
+package chapter7.inputoutput;
+
+/**
+ * Project: BeginningJava8LanguageFeatures
+ * FileName: Drink
+ * Date: 2017-06-16
+ * Time: 오전 9:28
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Drink {
+    protected String name;
+    protected double price;
+    public String getName() {
+        return name;
+    }
+    public double getPrice() {
+        return price;
+    }
+}
