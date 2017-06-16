7a916698afc4e2a8c4d4a64d620dcb3a4a21312d
sklee <hadesleeq@gmail.com>
2017-06-16 13:06:53 +0900

    A Whiskey Class

---
+++ src/main/java/chapter7/inputoutput/Whiskey.java
@@ -0,0 +1,17 @@
+package chapter7.inputoutput;
+
+/**
+ * Project: BeginningJava8LanguageFeatures
+ * FileName: Whiskey
+ * Date: 2017-06-16
+ * Time: 오후 1:06
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Whiskey extends Drink {
+    public Whiskey() {
+        this.name = "Whiskey";
+        this.price = 1.5;
+    }
+}
