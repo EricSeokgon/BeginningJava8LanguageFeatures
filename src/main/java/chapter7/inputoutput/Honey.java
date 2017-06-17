f45b53e0b493850fe467115d03e3c8da919d8731
hadesyi <leesk55@gmail.com>
2017-06-18 07:13:16 +0900

    A Honey Class, a Concrete Decorator

--- src/main/java/chapter7/inputoutput/Honey.java
+++ src/main/java/chapter7/inputoutput/Honey.java
@@ -4,5 +4,10 @@ package chapter7.inputoutput;
  * Created by hadeslee on 2017-06-17.
  */
 public class Honey extends DrinkDecorator {
+    public Honey(Drink drink) {
+        this.drink = drink;
+        this.name = "Honey";
+        this.price = 0.25;
+    }
 
 }
