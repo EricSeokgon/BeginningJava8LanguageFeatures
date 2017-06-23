08a6ab112b31490b6c638e76324788c32a584bb5
sklee <hadesleeq@gmail.com>
2017-06-23 11:00:18 +0900

    Redirecting Standard Outputs to a File

---
+++ src/main/java/chapter7/inputoutput/DummyStandardOutput.java
@@ -0,0 +1,18 @@
+package chapter7.inputoutput;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Project: BeginningJava8LanguageFeatures
+ * FileName: DummyStandardOutput
+ * Date: 2017-06-23
+ * Time: 오전 10:56
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class DummyStandardOutput extends OutputStream {
+    public void write(int b) throws IOException {
+    }
+}
