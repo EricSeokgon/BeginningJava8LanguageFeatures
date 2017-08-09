eb29b7326f89fd10738f6b4e882966d3349aded1
sklee <hadesleeq@gmail.com>
2017-08-09 09:18:38 +0900

    A Person Class

---
+++ src/main/java/chapter12/Clooections/Person.java
@@ -0,0 +1,62 @@
+package chapter12.Clooections;
+
+/**
+ * Project: BeginningJava8LanguageFeatures
+ * FileName: Person
+ * Date: 2017-08-09
+ * Time: 오전 9:16
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Person {
+    private int id;
+    private String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (id != person.id) return false;
+        return name != null ? name.equals(person.name) : person.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            '}';
+    }
+}
