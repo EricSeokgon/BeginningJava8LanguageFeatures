d1854a1416f9b9ec3cd2c9045def457a5f165118
hadesyi <leesk55@gmail.com>
2017-08-13 01:13:22 +0900

    Iterating Over the Elements in a List in Forward and Backward Directions

---
+++ src/main/java/chapter12/Clooections/ListIteratorTest.java
@@ -0,0 +1,36 @@
+package chapter12.Clooections;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * Created by hadeslee on 2017-08-13.
+ */
+public class ListIteratorTest {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("John");
+        list.add("Richard");
+        list.add("Donna");
+        list.add("Ken");
+        System.out.println("List: " + list);
+// Get the list iterator
+        ListIterator<String> iterator = list.listIterator();
+        System.out.println();
+        System.out.println("List Iterator in forward direction:");
+        while (iterator.hasNext()) {
+            int index = iterator.nextIndex();
+            String element = iterator.next();
+            System.out.println("Index=" + index + ", Element=" + element);
+        }
+        System.out.println();
+        System.out.println("List Iterator in backward direction:");
+// Reuse the iterator to iterate from the end to the beginning
+        while (iterator.hasPrevious()) {
+            int index = iterator.previousIndex();
+            String element = iterator.previous();
+            System.out.println("Index=" + index + ", Element=" + element);
+        }
+    }
+}
