223302b3e4cee6e18200d671207f66f43213699a
hadesyi <leesk55@gmail.com>
2017-08-19 00:55:11 +0900

    The Producer Class for a Blocking Queue

---
+++ src/main/java/chapter12/Clooections/BQProducer.java
@@ -0,0 +1,39 @@
+package chapter12.Clooections;
+
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * Created by hadeslee on 2017-08-19.
+ */
+public class BQProducer extends Thread {
+    private final BlockingQueue<String> queue;
+    private final String name;
+    private int nextNumber = 1;
+    private final Random random = new Random();
+    public BQProducer(BlockingQueue<String> queue, String name) {
+        this.queue = queue;
+        this.name = name;
+    }
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                String str = name + "-" + nextNumber;
+                System.out.println(name + " is trying to add: " +
+                    str + ". Remaining capacity: " +
+                    queue.remainingCapacity());
+                this.queue.put(str);
+                nextNumber++;
+                System.out.println(name + " added: " + str);
+// Sleep between 1 and 5 seconds
+                int sleepTime = (random.nextInt(5) + 1) * 1000;
+                Thread.sleep(sleepTime);
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
+}
